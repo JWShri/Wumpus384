e9a1995f35e79e97d47b38d06205dadd1c3713b5
Jamey S <34929284+JWShri@users.noreply.github.com>
2025-05-03 14:40:51 -0400

    Start
    loosely starting based off of game of life code

---
+++ Wumpus.py
@@ -0,0 +1,15 @@
+import random
+import unittest
+
+class Wumpus:
+        def __init__(self, rows, cols):
+
+
+#if __name__ == "__main__":
+#     initial_state = [
+#         [0, 1, 0, 0, 0],
+#         [0, 1, 0, 0, 0],
+#         [0, 1, 0, 0, 0],
+#         [0, 0, 0, 1, 0],
+#         [0, 0, 0, 0, 0]
+#     ]
